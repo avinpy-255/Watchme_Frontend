ae25c6232f98c60916bccc6a4f0104f164a80a96
Avinash <avinashchowdhury81@gmail.com>
2024-08-25 22:17:50 +0530

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+export default function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}
+//await window.Clerk.session.getToken({ template: "testing-template" });
